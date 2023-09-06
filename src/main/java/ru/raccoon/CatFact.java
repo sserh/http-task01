095c29227bbfa4120c4e2b3a10dd6c0fc6ec6603
sserh <serhios@bk.ru>
2023-09-06 10:03:51 +0700

    Initial commit

---
+++ src/main/java/ru/raccoon/CatFact.java
@@ -0,0 +1,55 @@
+package ru.raccoon;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CatFact {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private int upvotes;
+
+    public CatFact(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") int upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "CatFact" +
+                "\n id=" + id +
+                "\n text=" + text +
+                "\n type=" + type +
+                "\n user=" + user +
+                "\n upvotes=" + upvotes;
+    }
+}
